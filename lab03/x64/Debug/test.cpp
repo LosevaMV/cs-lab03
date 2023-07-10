399f5a6e49c0a7a57c670b5f90445d71bc88bd0f
Maria <lo5eva.maria@yandex.ru>
2023-07-10 13:20:24 +0300

    Модульные тесты

---
+++ lab03/x64/Debug/test.cpp
@@ -0,0 +1,50 @@
+﻿#include "histogram.h"
+#include <cassert>
+
+void test_positive() {
+    double min = 0;
+    double max = 0;
+    find_minmax({ 1, 2, 3 }, min, max);
+    assert(min == 1);
+    assert(max == 3);
+}
+
+void test_negative() {
+    double min = 0;
+    double max = 0;
+    find_minmax({ -1, -2, -3 }, min, max);
+    assert(min == -3);
+    assert(max == -1);
+}
+
+void test_same() {
+    double min = 0;
+    double max = 0;
+    find_minmax({ 5, 5, 5 }, min, max);
+    assert(min == 5);
+    assert(max == 5);
+}
+
+void test_one() {
+    double min = 0;
+    double max = 0;
+    find_minmax({ 7 }, min, max);
+    assert(min == 7);
+    assert(max == 7);
+}
+
+void test_empty() {
+    double min = 0;
+    double max = 0;
+    find_minmax({}, min, max);
+    assert(min == 0);
+    assert(max == 0);
+}
+
+int main() {
+    test_positive();
+    test_negative();
+    test_same();
+    test_one();
+    test_empty();
+}
